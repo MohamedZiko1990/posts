466990905444878c9a45daf1b6cbc282068f3de9
Khalafallah <Mohamed.khalafallah8@gmail.com>
2020-02-04 16:59:08 +0200

    getting posts and comments and construct new tables

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Users from "./Users";
+import Posts from "./Posts";
+import Comments from "./Comments";
 import axios from "axios";
 
 class App extends React.Component {
-  state = { usersData: null };
+  state = { usersData: null, postsData: null, commentsData: null };
 
   componentDidMount = () => {
-    const a = axios.get(" https://jsonplaceholder.typicode.com/posts").then(
+    axios.get(" https://jsonplaceholder.typicode.com/posts").then(
       response => {
-        console.log(response.data);
         this.setState({ usersData: response.data });
       },
       error => {
@@ -17,27 +18,57 @@ class App extends React.Component {
     );
   };
 
-  //   componentDidUpdate = () => {
-  //     console.log(this.state.usersData);
-  //   };
-
-  onClickUserId = () => {
-    console.log("clicked user id");
+  onClickUserId = (event, userId) => {
+    event.preventDefault();
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+      .then(
+        response => {
+          this.setState({ postsData: response.data });
+        },
+        error => {
+          console.log(error);
+        }
+      );
   };
 
-  onClickPostId = () => {
-    console.log("clicked post id");
+  onClickPostId = (event, postId) => {
+    event.preventDefault();
+    axios
+      .get(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
+      .then(
+        response => {
+          this.setState({ commentsData: response.data });
+        },
+        error => {
+          console.log(error);
+        }
+      );
   };
 
   render() {
     return (
       <div className="container">
-        <h3>Posts Table:</h3>
-        <Users
-          data={this.state.usersData}
-          onClickUser={this.onClickUserId}
-          onClickPost={this.onClickPostId}
-        />
+        <div className="row">
+          <h3>Posts Table:</h3>
+          <Users
+            data={this.state.usersData}
+            onClickUser={this.onClickUserId}
+            onClickPost={this.onClickPostId}
+          />
+        </div>
+        {this.state.postsData !== null && (
+          <div className="row pt-2">
+            <h3>Posts/User Table:</h3>
+            <Posts data={this.state.postsData} />
+          </div>
+        )}
+        {this.state.commentsData !== null && (
+          <div className="row pt-2">
+            <h3>Comments/Post Table:</h3>
+            <Comments data={this.state.commentsData} />
+          </div>
+        )}
       </div>
     );
   }
---
+++ src/Components/Comments.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Users.css";
+
+const Comments = props => {
+  var usersData = "";
+  if (props.data !== null) {
+    usersData = props.data.map(data => {
+      return (
+        <tr>
+          <td className="text-center">{data.postId}</td>
+          <td className="text-center">{data.id}</td>
+          <td>{data.name}</td>
+          <td>{data.email}</td>
+          <td>{data.body}</td>
+        </tr>
+      );
+    });
+  }
+
+  return (
+    <div className="container border border-primary span mt-3 pl-0 pr-0">
+      <table className="table table-bordered table-sm table-striped">
+        <thead>
+          <tr>
+            <th scope="col" className="text-center">
+              PostId
+            </th>
+            <th scope="col" className="text-center">
+              Id
+            </th>
+            <th scope="col" className="text-center">
+              Name
+            </th>
+            <th scope="col" className="text-center">
+              E-mail
+            </th>
+            <th scope="col" className="text-center">
+              Body
+            </th>
+          </tr>
+        </thead>
+        <tbody>{usersData}</tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Comments;
